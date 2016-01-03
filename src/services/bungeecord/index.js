290ce0b63a6872266b5547fcd9d4dc4ac9776e8d
Dane Everitt <dane@daneeveritt.com>
2016-01-03 18:09:03 -0500

    Adds bungeecord as a service, fixes minecraft configuration

---
+++ src/services/bungeecord/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Pterodactyl Daemon
+ * Copyright (c) 2015 Dane Everitt <dane@daneeveritt.com>
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+const rfr = require('rfr');
+
+const Configuration = rfr('src/services/bungeecord/main.json');
+const Core = rfr('src/services/index.js');
+
+class Service extends Core {
+    constructor(server) {
+        super(server, Configuration);
+    }
+
+    onPreflight(next) {
+        return super.onPreflight(next);
+    }
+
+    onStart(next) {
+        return super.onStart(next);
+    }
+
+    onConsole(data) {
+        return super.onConsole(data);
+    }
+
+    onStop(next) {
+        return super.onStop(next);
+    }
+
+    sanitizeSocketData(data) {
+        return super.sanitizeSocketData(data);
+    }
+
+}
+
+module.exports = Service;
