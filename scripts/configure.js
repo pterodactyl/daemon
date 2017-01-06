2045dbe389475b2cb523e91da6f69441a572e993
Jakob Schrettenbrunner <dev@schrej.net>
2017-01-07 00:50:02 +0100

    config script: require the panel url

--- scripts/configure.js
+++ scripts/configure.js
@@ -1,33 +1,45 @@
 const _ = require('lodash');
 const inquirer = require('inquirer');
+//const request = require('request');
+const url = require('url');
 
 const argv = require('yargs')
-  .usage('npm run configure -- <arguments>')
-  .describe('n', 'The name of the node to configure')
-  .alias('n', 'node-name')
-  .describe('t', 'An API token to use. Allows to skip login')
-  .alias('t', 'api-token')
-  .describe('u', 'An admin username to login with. Avoids API token use')
-  .alias('u', 'username')
-  .describe('p', 'An admin password to login with. Not recommended, use a token or interactive login')
-  .alias('p', 'password')
-  .describe('h', 'Show this help')
-  .alias('h', 'help')
-  .help('h')
-  .argv;
+    .usage('npm run configure -- <arguments>')
+    .describe('p', 'The url to the panel the node should be added to')
+    .alias('p', 'panel-url')
+    .describe('n', 'The name of the node to configure')
+    .alias('n', 'node-name')
+    .describe('t', 'An API token to use. Allows to skip login')
+    .alias('t', 'api-token')
+    .describe('u', 'An admin username to login with. Avoids API token use')
+    .alias('u', 'username')
+    .describe('password', 'An admin password to login with. Not recommended, use a token or interactive login')
+    .describe('h', 'Show this help')
+    .alias('h', 'help')
+    .help('h')
+    .argv;
 
 const options = {
+    panelurl: argv.p,
     nodename: argv.n,
     token: argv.t,
     username: argv.u,
-    password: argv.p,
+    password: argv.password,
 };
 
 inquirer.prompt([
     {
+        name: 'panelurl',
+        type: 'string',
+        message: 'Url of the panel to add the node to:',
+        when: () => _.isUndefined(options.panelurl),
+        validate: input => (input === '' ? 'The panel url is required' : true),
+        format: input => url.format(url.parse(input)),
+    },
+    {
         name: 'nodename',
         type: 'string',
-        message: 'The name of the node to configure:',
+        message: 'Name of the node to configure:',
         when: () => _.isUndefined(options.nodename),
         validate: input => (input === '' ? 'The node name is required' : true),
     },
@@ -43,13 +55,13 @@ inquirer.prompt([
         type: 'string',
         message: 'An admin username:',
         when: answers => _.isUndefined(options.token) && answers.token === '' && _.isUndefined(options.username),
-        validate: input => (input === '' ? 'Either a token or a user is required!' : true),
+        validate: input => (input === '' ? 'Either a token or a user is required' : true),
     },
     {
         name: 'password',
         type: 'password',
         message: 'Password:',
-        when: answers => _.isString(options.username) || _.isString(answers.username),
+        when: answers => _.isUndefined(options.password) && (_.isString(options.username) || _.isString(answers.username)),
         validate: input => (input === '' ? 'A password is required' : true),
     },
 ]).then(answers => {
