9a5b192827b102c8c882d4fbfd6d73143f13e90c
Jakob Schrettenbrunner <dev@schrej.net>
2017-01-08 01:01:42 +0100

    configure scripts now exists on parameter errors some refactoring and comments

--- scripts/configure.js
+++ scripts/configure.js
@@ -20,22 +20,10 @@ const regex = {
     ipv4: new RegExp(/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}/),
 };
 
-const argv = require('yargs')
-.usage('npm run configure -- <arguments>')
-    .describe('p', 'The panel url to pull the configuration from')
-    .alias('p', 'panel-url')
-    .describe('t', 'The configuration token')
-    .alias('t', 'token')
-    .boolean('o')
-    .describe('o', 'Overwrite existing configuration file')
-    .alias('o', 'overwrite')
-    .alias('h', 'help')
-    .help('h').argv;
-
 const params = {
     panelurl: {
         name: 'Panel url',
-        value: argv.p,
+        cmdoption: 'p',
         validate: value => {
             if (value === '') {
                 return 'The panel url is required';
@@ -48,7 +36,7 @@ const params = {
     },
     token: {
         name: 'Configuration token',
-        value: argv.t,
+        cmdoption: 't',
         validate: value => {
             if (value === '') {
                 return 'A configuration token is required.';
@@ -61,91 +49,118 @@ const params = {
     },
     overwrite: {
         name: 'Overwrite',
-        value: argv.o,
+        cmdoption: 'c',
     },
 };
 
-// Validate and filter the provided command line params
-function validateParams() {
-    _.forEach(params, (option, key) => {
-        if (!_.isUndefined(option.value)) {
-            if (_.isFunction(option.validate)) {
-                const valResult = option.validate(option.value);
-                if (valResult !== true) {
-                    log.error(`${option.name}: ${valResult}`);
-                    params[key].value = undefined;
-                    return;
-                }
+/**
+ * yargs compatible method for validating the arguments
+ */
+function checkParams(arg) {
+    _.forEach(params, param => {
+        if (_.has(arg, param.cmdoption)) {
+            const valResult = param.validate(arg[param.cmdoption]);
+            if (valResult !== true) {
+                throw new Error(`${param.name}: ${valResult}`);
             }
         }
     });
+    return true;
 }
 
-function main() {
-    const configExists = fs.existsSync(configFilePath);
+// Parse provided arguments with yargs for insanly easier usage
+const argv = require('yargs')
+    .usage('npm run configure -- <arguments>')
+    .describe('p', 'The panel url to pull the configuration from')
+    .alias('p', 'panel-url')
+    .describe('t', 'The configuration token')
+    .alias('t', 'token')
+    .boolean('o')
+    .describe('o', 'Overwrite existing configuration file')
+    .alias('o', 'overwrite')
+    .alias('h', 'help')
+    .check(checkParams)
+    .help('h')
+    .fail((msg, err, yargs) => {
+        log.error(err.message);
+        log.info(yargs.help());
+        process.exit();
+    })
+    .argv;
 
-    if (configExists) log.debug('Config already exists.');
+// Put the provided values into the overly complicated params object
+_.forEach(params, (param, key) => {
+    if (_.has(argv, param.cmdoption)) {
+        params[key].value = argv[param.cmdoption];
+    }
+});
 
-    inquirer.prompt([
-        {
-            name: 'panelurl',
-            type: 'string',
-            message: 'Url of the panel to add the node to:',
-            when: () => _.isUndefined(params.panelurl.value),
-            validate: params.panelurl.validate,
-        }, {
-            name: 'token',
-            type: 'string',
-            message: 'A configuration token to use:',
-            when: () => _.isUndefined(params.token.value),
-            validate: params.token.validate,
-        }, {
-            name: 'overwrite',
-            type: 'confirm',
-            default: false,
-            message: 'Overwrite existing configuration?',
-            when: () => !params.overwrite.value && configExists,
-        },
-    ]).then(answers => {
-        _.forEach(answers, (answer, key) => {
-            params[key].value = answer;
-        });
+// Check if the configuration file exists already
+const configExists = fs.existsSync(configFilePath);
+if (configExists) log.debug('Config already exists.');
 
-        if (!params.overwrite.value && configExists) {
-            log.error('Configuration already exists. Aborting.');
-            return;
-        }
+// Interactive questioning of missing parameters
+inquirer.prompt([
+    {
+        name: 'panelurl',
+        type: 'string',
+        message: 'Url of the panel to add the node to:',
+        when: () => _.isUndefined(params.panelurl.value),
+        validate: params.panelurl.validate,
+    }, {
+        name: 'token',
+        type: 'string',
+        message: 'A configuration token to use:',
+        when: () => _.isUndefined(params.token.value),
+        validate: params.token.validate,
+    }, {
+        // Will only be asked if file exists and no overwrite flag is set
+        name: 'overwrite',
+        type: 'confirm',
+        default: false,
+        message: 'Overwrite existing configuration?',
+        when: () => !params.overwrite.value && configExists,
+    },
+]).then(answers => {
+    // Overwrite the values in the overly complicated params object
+    _.forEach(answers, (answer, key) => {
+        params[key].value = answer;
+    });
 
-        log.info('Trying to fetch configuration from the panel...');
+    // If file exists and no overwrite wanted error and exit.
+    if (!params.overwrite.value && configExists) {
+        log.error('Configuration already exists. Aborting.');
+        process.exit();
+    }
 
-        request(`${params.panelurl.value}/remote/configuration/${params.token.value}`, (error, response, body) => {
-            if (!error) {
-                const jsonBody = JSON.parse(body);
-                if (response.statusCode === 200) {
-                    log.info('Writing configuration file...');
+    // Fetch configuration from the panel
+    log.info('Trying to fetch configuration from the panel...');
+    request(`${params.panelurl.value}/remote/configuration/${params.token.value}`, (error, response, body) => {
+        if (!error) {
+            // response should always be JSON
+            const jsonBody = JSON.parse(body);
 
-                    fs.writeFile(configFilePath, JSON.stringify(jsonBody, null, 4), err => {
-                        if (err) {
-                            log.error('Failed to write configuration file.');
-                        } else {
-                            log.info('Configuration written successfully.');
-                        }
-                    });
-                } else if (response.statusCode === 403) {
-                    if (jsonBody.error === 'token_invalid') {
-                        log.error('The token you used is invalid.');
-                    } else if (jsonBody.error === 'token_expired') {
-                        log.error('The token you used is expired.');
+            if (response.statusCode === 200) {
+                log.info('Writing configuration file...');
+
+                fs.writeFile(configFilePath, JSON.stringify(jsonBody, null, 4), err => {
+                    if (err) {
+                        log.error('Failed to write configuration file.');
                     } else {
-                        log.error('An unknown error occured!', body);
+                        log.info('Configuration written successfully.');
                     }
+                });
+            } else if (response.statusCode === 403) {
+                if (jsonBody.error === 'token_invalid') {
+                    log.error('The token you used is invalid.');
+                } else if (jsonBody.error === 'token_expired') {
+                    log.error('The token you used is expired.');
+                } else {
+                    log.error('An unknown error occured!', body);
                 }
-            } else {
-                log.error('Sorry. Something went wrong fetching the configuration.');
             }
-        });
+        } else {
+            log.error('Sorry. Something went wrong fetching the configuration.');
+        }
     });
-}
-
-validateParams();
-main();
+});
