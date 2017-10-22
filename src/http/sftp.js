930cb4509926da131586c3e9d549a9a93926d0ce
Dane Everitt <dane@daneeveritt.com>
2017-10-22 15:43:43 -0500

    Finalize SFTP actions, add support for changing file mode
    Also cleans up error handling to be more specific and easier to work with down the road.

--- src/http/sftp.js
+++ src/http/sftp.js
@@ -38,6 +38,7 @@ const SftpStream = Ssh2Streams.SFTPStream;
 const OPEN_MODE = Ssh2.SFTP_OPEN_MODE;
 const STATUS_CODE = Ssh2.SFTP_STATUS_CODE;
 
+const Package = rfr('package.json');
 const ConfigHelper = rfr('src/helpers/config.js');
 const Log = rfr('src/helpers/logger.js');
 const Servers = rfr('src/helpers/initialize.js').Servers;
@@ -68,20 +69,14 @@ class InternalSftpServer {
                         const sftp = a();
 
                         sftp.on('REALPATH', (reqId, location) => {
-                            Log.debug('REALPATH', clientContext.request_id, reqId, location);
-
                             let path = _.replace(Path.resolve(clientContext.server.path(), location), clientContext.server.path(), '');
                             if (_.startsWith(path, '/')) {
                                 path = path.substr(1);
                             }
-                            Log.debug('REALPATH [FINAL]', {
-                                filename: `/${path}`,
-                                longname: `drwxrwxrwx 1 foo foo 3 Dec 8 2009 /${path}`,
-                                attrs: {},
-                            });
+
                             return sftp.name(reqId, {
                                 filename: `/${path}`,
-                                longname: `drwxrwxrwx 1 foo foo 3 Dec 8 2009 /${path}`,
+                                longname: `drwxrwxrwx 2 foo foo 3 Dec 8 2009 /${path}`,
                                 attrs: {},
                             });
                         });
@@ -93,7 +88,12 @@ class InternalSftpServer {
                                         return sftp.status(reqId, STATUS_CODE.NO_SUCH_FILE);
                                     }
 
-                                    Log.error('STAT', err);
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a STAT operation in the SFTP server.', {
+                                        path: path,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+
                                     return sftp.status(reqId, STATUS_CODE.FAILURE);
                                 }
 
@@ -119,10 +119,6 @@ class InternalSftpServer {
 
                         sftp.on('READDIR', (reqId, handle) => {
                             const requestData = _.get(clientContext.handles, handle, null);
-                            if (_.isNull(requestData)) {
-                                Log.error('Unknown handle provided for READDIR');
-                                return sftp.status(reqId, STATUS_CODE.FAILURE);
-                            }
 
                             if (requestData.done) {
                                 return sftp.status(reqId, STATUS_CODE.EOF);
@@ -130,8 +126,16 @@ class InternalSftpServer {
 
                             this.handleReadDir(clientContext, requestData.path, (err, attrs) => {
                                 if (err) {
-                                    Log.error(err);
-                                    sftp.status(reqId, STATUS_CODE.FAILURE);
+                                    if (err.code === 'ENOENT') {
+                                        return sftp.status(reqId, STATUS_CODE.NO_SUCH_FILE);
+                                    }
+
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a READDIR operation in the SFTP server.', {
+                                        path: requestData.path,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+                                    return sftp.status(reqId, STATUS_CODE.FAILURE);
                                 }
 
                                 requestData.done = true;
@@ -167,7 +171,12 @@ class InternalSftpServer {
                                 data.writer = this.createWriter(clientContext, location);
                                 break;
                             default:
-                                Log.error('Received unknown SFTP flag.', { flag: SftpStream.flagsToString(flags), request: reqId });
+                                clientContext.server.log.warn('Received an unknown OPEN flag during SFTP operation.', {
+                                    path: location,
+                                    flag: SftpStream.flagsToString(flags),
+                                    identifier: clientContext.request_id,
+                                });
+
                                 return sftp.status(reqId, STATUS_CODE.OP_UNSUPPORTED);
                             }
 
@@ -179,10 +188,6 @@ class InternalSftpServer {
 
                         sftp.on('READ', (reqId, handle, offset, length) => {
                             const requestData = _.get(clientContext.handles, handle, null);
-                            if (_.isNull(requestData)) {
-                                Log.error('Unknown handle provided for READ');
-                                return sftp.status(reqId, STATUS_CODE.FAILURE);
-                            }
 
                             if (requestData.done) {
                                 return sftp.status(reqId, STATUS_CODE.EOF);
@@ -194,7 +199,12 @@ class InternalSftpServer {
                                 if ((err && err.code === 'IS_DIR') || done) {
                                     return sftp.status(reqId, STATUS_CODE.EOF);
                                 } else if (err) {
-                                    Log.error(err);
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a READ operation in the SFTP server.', {
+                                        path: requestData.path,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+
                                     return sftp.status(reqId, STATUS_CODE.FAILURE);
                                 }
 
@@ -202,21 +212,44 @@ class InternalSftpServer {
                             });
                         });
 
-                        sftp.on('SETSTAT', (reqId, path, attrs) => {
-                            sftp.status(reqId, STATUS_CODE.OK);
+                        sftp.on('SETSTAT', (reqId, location, attrs) => {
+                            if (_.isNull(_.get(attrs, 'mode', null))) {
+                                return sftp.status(reqId, STATUS_CODE.OK);
+                            }
+
+                            Fs.chmod(clientContext.server.path(location), attrs.mode, err => {
+                                if (err) {
+                                    if (err.code === 'ENOENT') {
+                                        return sftp.status(reqId, STATUS_CODE.NO_SUCH_FILE);
+                                    }
+
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a SETSTAT (CHMOD) operation in the SFTP server.', {
+                                        path: location,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+                                }
+
+                                return sftp.status(reqId, err ? STATUS_CODE.FAILURE : STATUS_CODE.OK);
+                            });
+                        });
+
+                        sftp.on('FSETSTAT', (reqId, handle, attrs) => {
+                            return sftp.emit('SETSTAT', clientContext.handles[handle].path, attrs);
                         });
 
                         sftp.on('WRITE', (reqId, handle, offset, data) => {
                             const requestData = _.get(clientContext.handles, handle, null);
-                            if (_.isNull(requestData)) {
-                                Log.error('Unknown handle provided for READ');
-                                return sftp.status(reqId, STATUS_CODE.FAILURE);
-                            }
 
                             // The writer is closed in the sftp.on('CLOSE') listener.
                             Fs.write(requestData.writer, data, 0, data.length, null, err => {
                                 if (err) {
-                                    Log.error(err);
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a WRITE operation in the SFTP server.', {
+                                        path: requestData.path,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+
                                     return sftp.status(reqId, STATUS_CODE.FAILURE);
                                 }
 
@@ -224,9 +257,15 @@ class InternalSftpServer {
                             });
                         });
 
-                        sftp.on('MKDIR', (reqId, path) => {
-                            Fs.ensureDir(clientContext.server.path(path), err => {
-                                if (err) Log.error(err);
+                        sftp.on('MKDIR', (reqId, location) => {
+                            Fs.ensureDir(clientContext.server.path(location), err => {
+                                if (err) {
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a MKDIR operation in the SFTP server.', {
+                                        path: location,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+                                }
 
                                 return sftp.status(reqId, err ? STATUS_CODE.FAILURE : STATUS_CODE.OK);
                             });
@@ -234,7 +273,20 @@ class InternalSftpServer {
 
                         sftp.on('RENAME', (reqId, oldPath, newPath) => {
                             clientContext.server.fs.move(oldPath, newPath, err => {
-                                if (err) Log.error(err);
+                                if (err) {
+                                    if (err.code === 'ENOENT') {
+                                        return sftp.status(reqId, STATUS_CODE.NO_SUCH_FILE);
+                                    }
+
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a RENAME operation in the SFTP server.', {
+                                        actions: {
+                                            from: oldPath,
+                                            to: newPath,
+                                        },
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+                                }
 
                                 return sftp.status(reqId, err ? STATUS_CODE.FAILURE : STATUS_CODE.OK);
                             });
@@ -246,9 +298,19 @@ class InternalSftpServer {
                             sftp.emit('RMDIR', reqId, path);
                         });
 
-                        sftp.on('RMDIR', (reqId, path) => {
-                            clientContext.server.fs.rm(path, err => {
-                                if (err) Log.error(err);
+                        sftp.on('RMDIR', (reqId, location) => {
+                            clientContext.server.fs.rm(location, err => {
+                                if (err) {
+                                    if (err.code === 'ENOENT') {
+                                        return sftp.status(reqId, STATUS_CODE.NO_SUCH_FILE);
+                                    }
+
+                                    clientContext.server.log.warn('An error occurred while attempting to perform a RMDIR operation in the SFTP server.', {
+                                        path: location,
+                                        exception: err,
+                                        identifier: clientContext.request_id,
+                                    });
+                                }
 
                                 return sftp.status(reqId, err ? STATUS_CODE.FAILURE : STATUS_CODE.OK);
                             });
@@ -272,7 +334,12 @@ class InternalSftpServer {
                                 if (!_.isUndefined(_.get(requestData, 'writer'))) {
                                     Fs.close(requestData.writer);
                                     clientContext.server.fs.chown(requestData.path, err => {
-                                        if (err) Log.error(err);
+                                        if (err) {
+                                            clientContext.server.log.warn('An error occurred while attempting to chown files on SFTP server.', {
+                                                exception: err,
+                                                identifier: clientContext.request_id,
+                                            });
+                                        }
                                     });
                                 }
 
@@ -284,7 +351,10 @@ class InternalSftpServer {
                     });
                 });
             }).on('error', err => {
-                Log.error('SFTP Subsystem Error!', err);
+                clientContext.server.log.error('An exception was encountered while handling the SFTP subsystem.', {
+                    exception: err,
+                    identifier: clientContext.request_id,
+                });
             });
         }).listen(Config.get('sftp.port', 2022), Config.get('sftp.ip', '0.0.0.0'), next);
     }
@@ -295,6 +365,26 @@ class InternalSftpServer {
 
     createWriter(ctx, path) {
         return Fs.openSync(ctx.server.path(path), 'w', 0o644);
+    }
+
+    formatFileMode(item) {
+        let longname = 'd';
+        if (!item.directory) {
+            longname = '-';
+        }
+
+        const permissions = _.split((item.mode & 0o777).toString(8), '');
+        _.forEach(permissions, el => {
+            el == 1 ? longname += '--x' : null; // eslint-disable-line
+            el == 2 ? longname += '-w-' : null; // eslint-disable-line
+            el == 3 ? longname += '-wx' : null; // eslint-disable-line
+            el == 4 ? longname += 'r--' : null; // eslint-disable-line
+            el == 5 ? longname += 'r-x' : null; // eslint-disable-line
+            el == 6 ? longname += 'rw-' : null; // eslint-disable-line
+            el == 7 ? longname += 'rwx' : null; // eslint-disable-line
+        });
+
+        return (item.directory) ? `${longname} 2` : `${longname} 1`;
     }
 
     handleReadDir(ctx, path, next) {
@@ -308,10 +398,8 @@ class InternalSftpServer {
                 const attrs = [];
                 _.forEach(files, item => {
                     const longFormat = Util.format(
-                        '%s 0 %i %i %d %s %s',
-                        (item.directory) ? 'drwxr-xr-x' : '-rw-r--r--',
-                        Config.get('docker.container.user', 1000),
-                        Config.get('docker.container.user', 1000),
+                        '%s container container %d %s %s',
+                        this.formatFileMode(item),
                         item.size,
                         Moment(item.created).format('MMM DD HH:mm'),
                         item.name,
@@ -321,8 +409,8 @@ class InternalSftpServer {
                         filename: item.name,
                         longname: longFormat,
                         attrs: {
-                            mode: (item.directory) ? Fs.constants.S_IFDIR | 0o755 : Fs.constants.S_IFREG | 0o644,
-                            permissions: (item.directory) ? 0o755 : 0o644,
+                            mode: item.mode,
+                            permissions: (item.mode & 0o777).toString(8),
                             uid: Config.get('docker.container.user', 1000),
                             gid: Config.get('docker.container.user', 1000),
                             size: item.size,
