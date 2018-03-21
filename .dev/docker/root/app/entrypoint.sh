3d04ce781344c1b8aecabd60b93e870beaf0d6d6
HoverEpic <pcsrvadm@gmail.com>
2018-03-21 16:08:07 +0100

    simplify docker entrypoint

--- .dev/docker/root/app/entrypoint.sh
+++ .dev/docker/root/app/entrypoint.sh
@@ -1,22 +1,16 @@
 #!/bin/ash
 set -e
 
-# Check if default command is used
-if [ "$1" = "/usr/local/bin/npm" ]; then
-    if [[ ! -z "$PANEL_URL" && ! -z "$TOKEN" ]]; then
-        echo "Configuring Pterodactyl Daemon container"
-        echo "Setting up Node remote connection"
-        npm run configure -- -p $PANEL_URL -t $TOKEN
+if [[ ! -z "$PANEL_URL" && ! -z "$TOKEN" ]]; then
+    echo "Configuring Pterodactyl Daemon container"
+    echo "Setting up Node remote connection"
+    npm run configure -- -p $PANEL_URL -t $TOKEN
 
-        if [ ! -S "/var/run/docker.sock" ]; then
-            echo "Docker daemon not found !"
-            exit 1
-        fi
-        echo "Configuration is done."
-    else
-        echo "Configuration failed, PANEL_URL and/or TOKEN missing from env."
+    if [ ! -S "/var/run/docker.sock" ]; then
+        echo "Docker daemon not found !"
         exit 1
     fi
+    echo "Configuration is done."
 fi
 
 exec "$@"
