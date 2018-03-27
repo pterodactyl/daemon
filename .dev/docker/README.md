7038edc818919b191197ad6dddd0834ff687dbf2
HoverEpic <pcsrvadm@gmail.com>
2018-03-27 13:24:41 +0200

    Docker : update entrypoint to filter setup/start case, update readme.

--- .dev/docker/README.md
+++ .dev/docker/README.md
@@ -5,11 +5,9 @@ execute the following command in project root
 ```docker build -t <repo>/<image>:<tag> .```
 
 ## Start
-if you use SSL Connection, do not forget to setup letsencrypt on the host
+If you use SSL Connection, do not forget to setup letsencrypt on the host
 ```
-docker run --rm \
---name=ptero-daemon \
---hostname ptero-daemon \
+docker run \
 -p 8080:8080 \
 -p 2022:2022 \
 -v /tmp/pterodactyl/:/tmp/pterodactyl/ \
@@ -19,8 +17,9 @@ docker run --rm \
 -v /etc/letsencrypt/:/etc/letsencrypt/ \
 -e PANEL_URL=<panel url> \
 -e TOKEN=<token> \
--ti <repo>/<image>:<tag>
+-d <repo>/<image>:<tag>
 ```
+If you choose to save the config, PANEL_URL and TOKEN will not be necessary on future starts.
 
 ## Ports
  - 8080 for management
@@ -33,6 +32,6 @@ docker run --rm \
  - `/app/config/` path of the pterodactyl daemon config, can be what you want
  - `/etc/letsencrypt/` path of the certbot certs for ssl/tls, needed only for ssl/tls
 
-## Environement variables (used for deploying new nodes, not necessary on )
+## Environement variables (used for deploying new nodes)
  - `PANEL_URL` the panel external address
- - `TOKEN` the linking token
+ - `TOKEN` the link token
--- .dev/docker/root/app/entrypoint.sh
+++ .dev/docker/root/app/entrypoint.sh
@@ -1,21 +1,29 @@
 #!/bin/ash
 set -e
 
+CONFIG="config/core.json"
+
 # Check if default command is used
 if [ "$1" = "/usr/bin/npm" ]; then
-    if [[ ! -z "$PANEL_URL" && ! -z "$TOKEN" ]]; then
-        echo "Configuring Pterodactyl Daemon container"
-        echo "Setting up Node remote connection"
-        npm run configure -- -p $PANEL_URL -t $TOKEN
 
-        if [ ! -S "/var/run/docker.sock" ]; then
+    if [ ! -z "$PANEL_URL" ] && [ ! -z "$TOKEN" ]; then
+        echo "] Configuring Pterodactyl Daemon container"
+
+        if [ -e $CONFIG ] && [ -z "$OVERWRITE" ]; then
+            echo "Configuration file $CONFIG exist and OVERWRITE not set, abording !"
+            exit 1
+        fi
+
+        npm run configure -- --panel-url="$PANEL_URL" --token="$TOKEN" --overwrite
+        chmod 775 $CONFIG
+
+        if [ -S "/var/run/docker.sock" ]; then
+            echo "] Docker daemon found !"
+        else
             echo "Docker daemon not found !"
             exit 1
         fi
-        echo "Configuration is done."
-    else
-        echo "Configuration failed, PANEL_URL and/or TOKEN missing from env."
-        exit 1
+        echo "] Configuration is done."
     fi
 fi
 
